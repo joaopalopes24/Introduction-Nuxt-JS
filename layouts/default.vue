7e713313f20e8feb34983c434a8e0e993df9a4b0
João Pedro Lopes <joaopalopes24@gmail.com>
2022-07-17 23:56:52 -0300

    feat(menu): adding nuxt link in menu buttons

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,9 +12,25 @@
               <div class="hidden sm:block sm:ml-6">
                 <div class="flex space-x-4">
                   <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                  <a href="#" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Home</a>
+                  <NuxtLink to="/"
+                            class="px-3 py-2 rounded-md text-sm font-medium"
+                            :class="{
+                              'bg-gray-900 text-white' : $route.name === 'index',
+                              'text-gray-300 hover:bg-gray-700 hover:text-white' : $route.name !== 'index',
+                            }"
+                  >
+                    Home
+                  </NuxtLink>
 
-                  <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Categorias</a>
+                  <NuxtLink to="/categories"
+                            class="px-3 py-2 rounded-md text-sm font-medium"
+                            :class="{
+                              'bg-gray-900 text-white' : $route.name === 'categories',
+                              'text-gray-300 hover:bg-gray-700 hover:text-white' : $route.name !== 'categories',
+                            }"
+                  >
+                    Categorias
+                  </NuxtLink>
                 </div>
               </div>
             </div>
